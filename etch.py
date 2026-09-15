15ed03f1257ede740cdb73bb2a9e21bb77d9433b
Dhruv Ravinuthala <dhruv.ravinuthala@gmail.com>
2026-09-15 00:51:27 -0500

    added bordering so it looks like a real etch-a-sketch! also added multiple turtles so that the entire screen wasn't clearred when space was pressed.

--- etch.py
+++ etch.py
@@ -1,58 +1,128 @@
-import turtle as t
+import turtle 
 import time
 
-s = t.Screen()
+s = turtle.Screen()
 s.setup(width= 800, height=600)
-t.listen()
+turtle.listen()
 
-coords = t.pos()
-x,y = coords
+coords = ""
+x,y = "", ""
 
+t = turtle.Turtle(visible = False)
+
+t.up()
+t.backward(400)
+t.setheading(90)
+t.backward(300)
+t.pencolor('red')
+t.down()
+t.setheading(0)
+t.fillcolor('red')
+
+t.begin_fill()
+t.forward(800)
+t.setheading(90)
+t.forward(100)
+t.setheading(180)
+t.forward(800)
+t.setheading(270)
+t.forward(100)
+t.end_fill()
+
+t.setheading(0)
+t.forward(75)
+t.begin_fill()
+t.setheading(90)
+t.forward(600)
+t.setheading(180)
+t.forward(75)
+t.setheading(270)
+t.forward(600)
+t.setheading(0)
+t.forward(75)
+t.end_fill()
+
+t.setheading(90)
+t.forward(600)
+t.begin_fill()
+t.setheading(0)
+t.forward(800)
+t.setheading(270)
+t.forward(100)
+t.setheading(180)
+t.forward(800)
+t.setheading(90)
+t.forward(100)
+t.end_fill()
+
+t.setheading(0)
+t.forward(725)
+t.begin_fill()
+t.setheading(270)
+t.forward(600)
+t.setheading(180)
+t.forward(75)
+t.setheading(90)
+t.forward(600)
+t.setheading(0)
+t.forward(75)
+t.end_fill()
+
+
+
+
+tu = turtle.Turtle(visible=False)
+tu.up()
+tu.setpos(0, 0)
+tu.down()
+tu.pencolor('black')
 
 def forward():
     global coords
     global x, y 
-    coords = t.pos()
+    coords = tu.pos()
     x,y = coords
-    if x <= 400:
-        t.setheading(0)
-        t.forward(10)
+    if x <= 300:
+        tu.setheading(0)
+        tu.forward(10)
 
 def backward():
     global coords
     global x, y 
-    coords = t.pos()
+    coords = tu.pos()
     x,y = coords
-    if x >= -400:
-        t.setheading(0)
-        t.backward(10)
+    if x >= -300:
+        tu.setheading(0)
+        tu.backward(10)
 
 def upwards():
     global coords
     global x, y 
-    coords = t.pos()
+    coords = tu.pos()
     x,y = coords
-    if y <= 300:    
-        t.setheading(90)
-        t.forward(10)
+    if y <= 180:    
+        tu.setheading(90)
+        tu.forward(10)
     
 
 def downwards():
     global coords
     global x, y 
-    coords = t.pos()
+    coords = tu.pos()
     x,y = coords
-    if y >= -300:
-        t.setheading(90)
-        t.backward(10)
+    if y >= -180:
+        tu.setheading(90)
+        tu.backward(10)
     
 
 def clear():
-    t.clear()
+    tu.clear()
 
-t.onkeypress(forward, 'Right')
-t.onkeypress(backward, 'Left')
-t.onkeypress(upwards, 'Up')
-t.onkeypress(downwards, 'Down')
-t.onkeypress(clear, 'space')
-t.done()
+
+print(x, y)
+turtle.onkeypress(forward, 'Right')
+turtle.onkeypress(backward, 'Left')
+turtle.onkeypress(upwards, 'Up')
+turtle.onkeypress(downwards, 'Down')
+turtle.onkeypress(clear, 'space')
+turtle.done()
